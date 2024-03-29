cc8119c43d58e395e151f3fccbbab8906a78e780
OXGN <cabarrubias.nevinharold@gmail.com>
2024-03-29 19:51:09 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import LoginPage from './Screens/Login';
-import SignupPage from './Screens/SignupPage';
-import UserProfilePage from './Screens/UserProfile';
-import UserProfileSettingsPage from './Screens/UserProfileSettings';
-import SignupConfirmationPage from './Screens/SignupPageConfirmation';
-import HomePage from './Screens/Home';
-import SignupTermsAndConditionPage from './Screens/SignupPageTermsAndAgreement';
-import GoogleMapRegisterSelectPlace from './Screens/GoogleMapRegisterSelectPlace';
-import GoogleMapRegisterImage from './Screens/GoogleMapRegisterImage';
-import GoogleMapRegisterInformation from './Screens/GoogleMapRegisterInformation'
-import GoogleMapSearch from './Screens/GoogleMapSearch';
-import GoogleMapRegisterImageConfirmation from './Screens/GoogleMapRegisterImageConfirmation'
-import GoogleMapSearchProfile from './Screens/GoogleMapSearchProfile'
-import AboutUs from './Screens/AboutUsPage';
-import Test from './Screens/Test'
+
+import AllTasks from './screens/tasks/AllTasks';
+import Finished from './screens/tasks/Finished';
+import InProgress from './screens/tasks/InProgress';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -24,27 +12,15 @@ export default function App() {
 return (
     <NavigationContainer>
         <Stack.Navigator
-            initialRouteName="GoogleMapSearch"
+            initialRouteName="AllTasks"
             screenOptions={{
                 headerShown: false,
                 animation: 'slide_from_right', // Set the animation option
             }}
         >
-            <Stack.Screen name="Test" component={Test} />
-            <Stack.Screen name="Login" component={LoginPage} />
-            <Stack.Screen name="Home" component={HomePage} />
-            <Stack.Screen name="GoogleMapRegister" component={GoogleMapRegisterSelectPlace} />
-            <Stack.Screen name="GoogleMapRegisterInformation" component={GoogleMapRegisterInformation} />
-            <Stack.Screen name="GoogleMapRegisterImageConfirmation" component={GoogleMapRegisterImageConfirmation} />
-            <Stack.Screen name="GoogleMapRegisterImage" component={GoogleMapRegisterImage} />
-            <Stack.Screen name="GoogleMapSearch" component={GoogleMapSearch}/>
-            <Stack.Screen name="GoogleMapSearchProfile" component={GoogleMapSearchProfile}/>
-            <Stack.Screen name="UserProfile" component={UserProfilePage}/>
-            <Stack.Screen name="UserProfileSetting" component={UserProfileSettingsPage}/> 
-            <Stack.Screen name="SignUp" component={SignupPage}/>
-            <Stack.Screen name="SignUpTermsAndCondtion" component={SignupTermsAndConditionPage}/>
-            <Stack.Screen name="SignupConfirmation" component={SignupConfirmationPage}/>
-            <Stack.Screen name="AboutUs" component={AboutUs}/>
+            <Stack.Screen name="AllTasks" component={AllTasks}/>
+            <Stack.Screen name="InProgress" component={InProgress}/>
+            <Stack.Screen name="Finished" component={Finished}/>
         </Stack.Navigator>
     </NavigationContainer>
     );
